778daa4ad7cb53bf7d318a59abfeef2172c8e918
R0Y4L <royalb9514@gmail.com>
2023-03-21 17:41:31 +0400

    Intial

---
+++ Client/Program.cs
@@ -0,0 +1,84 @@
+﻿using Client.Models;
+using System.Net.Sockets;
+using System.Text.Json;
+
+var client = new TcpClient("127.0.0.1", 1234);
+
+var stream = client.GetStream();
+
+var bw = new BinaryWriter(stream);
+var br = new BinaryReader(stream);
+
+var CommandText = string.Empty;
+var CommandParam = string.Empty;
+
+while (true)
+{
+    Console.WriteLine("Enter Command Text");
+    CommandText = Console.ReadLine();
+    Console.WriteLine("Enter Command Param");
+    CommandParam = Console.ReadLine();
+    if (string.IsNullOrEmpty(CommandText) || string.IsNullOrEmpty(CommandParam))
+    {
+        Console.WriteLine("You entered an error, please try again");
+        await Task.Delay(100);
+        continue;
+    }
+
+    var command = new Command
+    {
+        Text = CommandText,
+        Param = CommandParam
+    };
+
+    switch (command.Text.ToLower())
+    {
+        case Command.HELP:
+            {
+                var jsonStr = JsonSerializer.Serialize(command);
+                bw.Write(jsonStr);
+                await Task.Delay(50);
+                var response = br.ReadString();
+                Console.WriteLine(response);
+                break;
+            }
+        case Command.PROCLIST:
+            {
+                var jsonStr = JsonSerializer.Serialize(command);
+                bw.Write(jsonStr);
+                await Task.Delay(50);
+                var response = br.ReadString();
+                Console.WriteLine(response);
+                break;
+            }
+        case Command.RUN:
+            {
+                var jsonStr = JsonSerializer.Serialize(command);
+                bw.Write(jsonStr);
+                await Task.Delay(50);
+
+                var responseBool = br.ReadBoolean();
+                if (responseBool is true)
+                    Console.WriteLine("Process succesfully ended");
+                else
+                    Console.WriteLine("Process couldn't ended");
+                break;
+            }
+        case Command.KILL:
+            {
+                var jsonStr = JsonSerializer.Serialize(command);
+                bw.Write(jsonStr);
+                await Task.Delay(50);
+
+                var responseBool = br.ReadBoolean();
+                if (responseBool is true)
+                    Console.WriteLine("Process succesfully ended");
+                else
+                    Console.WriteLine("Process couldn't ended");
+                break;
+            }
+        default:
+            break;
+    }
+
+}
---
+++ Server/Program.cs
@@ -0,0 +1,91 @@
+﻿using Server.Models;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 1234);
+
+server.Start();
+
+while (true)
+{
+    var client = server.AcceptTcpClient();
+
+    var stream = client.GetStream();
+    var binaryWrite = new BinaryWriter(stream);
+    var binaryRead = new BinaryReader(stream);
+
+    while (true)
+    {
+        var strRead = binaryRead.ReadString();
+
+        var command = JsonSerializer.Deserialize<Command>(strRead);
+
+        if (command is null)
+            return;
+
+
+        switch (command.Text.ToLower())
+        {
+            case Command.HELP:
+                string message = @"proclist ------ see all processes 
+kill <process name> ------ end process
+run <process name> ------ run process";
+                binaryWrite.Write(message);
+                binaryWrite.Flush();
+                break;
+
+            case Command.PROCLIST:
+                var list = Process.GetProcesses();
+                var names = list.Select(p => p.ProcessName);
+                var jsonList = JsonSerializer.Serialize(names);
+                binaryWrite.Write(jsonList);
+                binaryWrite.Flush();
+                break;
+
+            case Command.RUN:
+                if (command.Param is null)
+                {
+                    binaryWrite.Write(false);
+                    break;
+                }
+                try
+                {
+                    Process.Start(command.Param);
+                    binaryWrite.Write(true); ;
+                }
+                catch (Exception) {
+                    
+                    binaryWrite.Write(false);  
+                }
+                break;
+
+
+            case Command.KILL:
+                if (command.Param is null)
+                {
+                    binaryWrite.Write(false);
+                    break;
+                }
+                var processName = Process.GetProcessesByName(command.Param);
+                if (processName.Length == 0)
+                {
+                    binaryWrite.Write(false);
+                    break;
+                }
+                foreach (var item in processName)
+                {
+
+                    item.Kill();
+                    binaryWrite.Write(true);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+ 
